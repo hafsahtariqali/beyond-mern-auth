81096d011ef201190c85717cf51e678bad5ef741
hafsahtariqali <alihafsatariq@gmail.com>
2025-07-17 14:25:02 +0500

    fixed errors

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -21,8 +21,8 @@ export const registerUser = async(req, res) => {
     const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? 'none': 'strict',
+      secure: true,
+      sameSite: 'None',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -61,8 +61,8 @@ export const loginUser = async(req, res) => {
     const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? 'none': 'strict',
+      secure: true,
+      sameSite: 'None',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 

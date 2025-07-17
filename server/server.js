7da17c215dc19c54d63aa1aac6caf71fe2cc4586
hafsahtariqali <alihafsatariq@gmail.com>
2025-07-17 13:16:39 +0500

    updates urls for frontend

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 4000;
 
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173', 'https://beyond-mern-auth.vercel.app/'];
 
 app.use(express.json());
 app.use(cookieParser());
